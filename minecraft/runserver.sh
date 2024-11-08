c20c72ce5736fab70499b2311f3ea92ad1627b24
SollyBunny <sollybunn@gmail.com>
2024-11-08 18:50:32 +0000

    update runserver.sh

--- minecraft/runserver.sh
+++ minecraft/runserver.sh
@@ -3,6 +3,6 @@
 # Add custom JVM arguments to the user_jvm_args.txt
 # Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
 #  pass them to this script directly
-JAVA=/usr/bin/java
+JAVA=$(readlink -f $(which java))
 sudo setcap 'cap_net_bind_service=+ep' $JAVA
 $JAVA @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.19.2-43.4.0/unix_args.txt nogui "$@"
